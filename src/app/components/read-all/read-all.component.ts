5703ab01482ac901b3ff88f7cd703d5801ed3b5f
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-07-25 19:00:35 -0300

    Adicionado método de delete para livro

--- src/app/components/read-all/read-all.component.ts
+++ src/app/components/read-all/read-all.component.ts
@@ -33,4 +33,13 @@ export class ReadAllComponent implements OnInit {
     })
   }
 
+  delete(id: any):void {
+    this.service.delete(id).subscribe((resposta) => {
+      if(resposta === null) {
+        this.service.message('Task deletada com sucesso!');
+        this.list = this.list.filter(livro => livro.id !== id);
+      }
+    })
+  }
+
 }
