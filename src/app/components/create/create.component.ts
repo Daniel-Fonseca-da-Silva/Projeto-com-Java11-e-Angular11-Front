469d90213ee6364d21ef4368834c2c1a23e04813
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-07-26 00:45:50 -0300

    Implementado botão cancelar

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  cancelar(): void {
+    this.router.navigate(['']);
   }
 
 }
